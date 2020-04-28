5f259ddb790d8d8c38c63568450ffe66ea5cf7dc
Will Gibson <WillGibson@users.noreply.github.com>
2020-04-28 09:36:21 +0100

    Output Node.js version & add --recreate-containers (#3)

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -2,5 +2,13 @@
 
 set -e
 
-docker-compose up -d
+arguments="$*"
+dockerComposeArgs=""
+
+if [[ "$arguments" == *"--recreate-containers"* ]]; then
+   dockerComposeArgs="$dockerComposeArgs --force-recreate"
+fi
+
+docker-compose up -d $dockerComposeArgs
+docker-compose exec node node -v
 docker-compose exec node bash
